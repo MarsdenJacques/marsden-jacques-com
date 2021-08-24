d7dc0464e16072367d08c5e354c08dfd895a718c
MarsdenJacques <marsdensgardens@gmail.com>
2021-08-24 14:49:21 +0900

    changed something in the svg I forgot to change to JSX-friendly camelcase, hopefully that's the only issue

--- components/circle-text-svg.js
+++ components/circle-text-svg.js
@@ -3,7 +3,7 @@ export default function CircleTextSVG({text}){
     return(
         <div className = {styles.container}>
             <div className = {styles.svg}>
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="300px" height="300px" viewBox="0 0 300 300" enable-background="new 0 0 300 300" xmlSpace="preserve">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="300px" height="300px" viewBox="0 0 300 300" enableBackground="new 0 0 300 300" xmlSpace="preserve">
                     <defs>
                         <path id="circlePath" d="M 130, 151 m -60, 0 a 60,60 0 0,1 160,0 a 60,60 0 0,1 -160,0 "/>
                     </defs>
