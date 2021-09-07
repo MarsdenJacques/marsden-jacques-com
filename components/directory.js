7ee7bd539a03af0ff37cdb3e1d7bc49fcecbf200
Marsden Jacques <marsdensgardens@gmail.com>
2021-09-07 13:05:18 +0900

    nav styling change

--- components/directory.js
+++ components/directory.js
@@ -12,18 +12,21 @@ export default function Directory({currentSection, currentLanguage, Scroll}){
     function ToggleOnPortfolio(){
         setShowPortfolio('')
     }
+    function style(index){
+        return index === currentSection ? styles.selector + ' ' + styles.active : styles.selector
+    }
     //need japanese language style
     return(
         <div className = {styles.container} onMouseLeave = {()=> ToggleOffPortfolio()}>
             <div className = {styles.subContainer}>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(2)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[2]}</p></div></div>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(3)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[3]}</p></div></div>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(4)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[4]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(2)} onClick = {()=>Scroll(2)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[2]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(3)} onClick = {()=>Scroll(3)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[3]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(4)} onClick = {()=>Scroll(4)}><p className = {styles.link + showPortfolio}>{currentLanguage.banner[4]}</p></div></div>
             </div>
             <div className = {styles.subContainer}>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(0)}><p className = {styles.link} onMouseEnter = {()=>ToggleOffPortfolio()}>{currentLanguage.banner[0]}</p></div></div>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(1)}><p className = {styles.link} onMouseEnter = {()=>ToggleOnPortfolio()}>{currentLanguage.banner[1]}</p></div></div>
-                <div className = {styles.textContainer}><div className = {styles.selector} onClick = {()=>Scroll(5)}><p className = {styles.link} onMouseEnter = {()=>ToggleOffPortfolio()}>{currentLanguage.banner[5]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(0)} onClick = {()=>Scroll(0)}><p className = {styles.link} onMouseEnter = {()=>ToggleOffPortfolio()}>{currentLanguage.banner[0]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(1)} onClick = {()=>Scroll(1)}><p className = {styles.link} onMouseEnter = {()=>ToggleOnPortfolio()}>{currentLanguage.banner[1]}</p></div></div>
+                <div className = {styles.textContainer}><div className = {style(5)} onClick = {()=>Scroll(5)}><p className = {styles.link} onMouseEnter = {()=>ToggleOffPortfolio()}>{currentLanguage.banner[5]}</p></div></div>
             </div>
         </div>
     )
