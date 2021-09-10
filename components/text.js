de7e7592cc20db9c66b2c6668bb9e6e3abe1ef63
Marsden Jacques <marsdensgardens@gmail.com>
2021-09-10 19:47:32 +0900

    added tsushima art fantasia to the websites list

--- components/text.js
+++ components/text.js
@@ -8,19 +8,20 @@ const English = {
     'The currently hosted websites that I was commissioned to develop, or made as a personal project.',
     'Check out some of my games and game related projects!','Here are my two published papers. My research work is focused on the fields of Combinatorics and Algorithms in Computer Science.',
     'Feel free to get in touch with me on any of these platforms, or email me at: marsdenjacques@gmail.com!'],
-    images: [['/me1.jpg','/github.png','/resume.jpg'],['/hac.jpg','/tetronivibesweb.jpg','/wogc.jpg'],['/hac.jpg','/notestodo.jpg','/githubsite.jpg'],['/tetronivibesweb.jpg','/conway.jpg','/itchio.svg','/kac.jpg','/miyauchi.jpg'],['/wogc.jpg','/wouc.jpg','/dblp.png'],['/linkedin.png','/email.png']],
+    images: [['/me1.jpg','/github.png','/resume.jpg'],['/hac.jpg','/tetronivibesweb.jpg','/wogc.jpg'],['/hac.jpg','/notestodo.jpg','/tsushima.jpg','/githubsite.jpg'],['/tetronivibesweb.jpg','/conway.jpg','/itchio.svg','/kac.jpg','/miyauchi.jpg'],['/wogc.jpg','/wouc.jpg','/dblp.png'],['/linkedin.png','/email.png']],
     imageText: [['This is me', 'Some images are links! (like this one for my Github)','Here is my resume'], 
     ['I make websites...', 'Games...','And do research!'], 
-    ['Hiroshima Art Center homepage made with Gatsby', 'A simple notes and todo app made with NextJS, NodeJS, and MySQL', 'My github.io website playground'], 
+    ['Hiroshima Art Center homepage made with Gatsby', 'A simple notes and todo app made with NextJS, NodeJS, and MySQL','Tsushima Art Fantasia 2021 Exhibition Site','My github.io website playground'], 
     ['TetromiVIBES (with C# in Unity)', 'Conway\'s Game of Life made with Javascript', 'Check out my itch.io!','I made an AR installation for the Kyoto Art Center...','And also for Art Gallery Miyauchi in Hiroshima'],
     ['Generating Gray codes for weak orders in constant amortized time', 'Greedy Universal Cycle Constructions for Weak Orders','DBLP online bibliography'],
     ['Linkedin', 'Click to email me!']],
     link: [['','https://github.com/MarsdenJacques','https://drive.google.com/file/d/1QX0TggkcIaQD4ZF1r5c0kvhsx4jLfceg/view?usp=sharing'],
     ['/#Websites','/#Games','/#Research'],
-    ['https://hiroshimageisen.netlify.app/','https://marsdensqlnotesapp.netlify.app','https://marsdenjacques.github.io/'],
+    ['https://hiroshimageisen.netlify.app/','https://marsdensqlnotesapp.netlify.app','https://tsushimaartfantasia.netlify.app/','https://marsdenjacques.github.io/'],
     ['https://marsdenjacques.itch.io/tetromivibes','https://marsdenjacques.github.io/2d-game-of-life/','https://marsdenjacques.itch.io/','https://www.kac.or.jp/wp-content/uploads/63a954ded52fd6bc624a1746aa2ffbce.pdf',''],
     ['https://www.sciencedirect.com/science/article/pii/S0012365X20301783?via%3Dihub','https://drive.google.com/file/d/1nZP3B6fD0kmuakXtf6rMIdP8ZzX9VdqV/view','https://dblp.org/pid/257/1379.html'],
-    ['https://www.linkedin.com/in/marsdenjacques/','mailto:marsdenjacques@gmail.com']]
+    ['https://www.linkedin.com/in/marsdenjacques/','mailto:marsdenjacques@gmail.com']],
+    color: ['','','','','','']
 }
 const Japanese = {
     language: '日本語',
@@ -31,23 +32,24 @@ const Japanese = {
     '作ったのゲームとゲーム見たいなプログラムをぜひやって見て下さい',
     '僕の研究はコンピュータサイエンスの組合せ数学とアルゴリズムに焦点を当てている',
     'このプラットフォームで僕に連絡しててもいいか、marsdenjacques@gmail.comでemail送ってもいい！よろしくお願いします'],
-    images: [['/me1.jpg','/github.png','/resume.jpg'],['/hac.jpg','/tetronivibesweb.jpg','/wogc.jpg'],['/hac.jpg','/notestodo.jpg','/githubsite.jpg'],['/tetronivibesweb.jpg','/conway.jpg','/itchio.svg','/kac.jpg','/miyauchi.jpg'],['/wogc.jpg','/wouc.jpg','/dblp.png'],['/linkedin.png','/email.png']],
+    images: [['/me1.jpg','/github.png','/resume.jpg'],['/hac.jpg','/tetronivibesweb.jpg','/wogc.jpg'],['/hac.jpg','/notestodo.jpg','/tsushima.jpg','/githubsite.jpg'],['/tetronivibesweb.jpg','/conway.jpg','/itchio.svg','/kac.jpg','/miyauchi.jpg'],['/wogc.jpg','/wouc.jpg','/dblp.png'],['/linkedin.png','/email.png']],
     imageText: [['僕', 'いくつかの画像はリンク！例えばこの画像は僕のGitHubのリンク','僕の履歴書'], 
     ['ウエブサイト。。。', 'ゲーム。。。','と研究全部やっている！'], 
-    ['Gatsbyで作った広島芸術センターのホームページ', 'NextJS、 NodeJS、とMySQLで作ったノート書くのウエブアプリ', '僕のgithub.ioの遊んで作っているのサイト'], 
+    ['Gatsbyで作った広島芸術センターのホームページ', 'NextJS、 NodeJS、とMySQLで作ったノート書くのウエブアプリ','対馬アートファンタジアの2021年展覧会サイト', '僕のgithub.ioの遊んで作っているのサイト'], 
     ['C#とUnityで作ったTetromiVIBESて言うゲーム', 'Javascriptで作ったConwayのライフゲーム', '僕のitch.ioはこちら!','京都アートセンターにAR展示つくった。。。','とアートギャラリーミヤウチにも作った'],
     ['Generating Gray codes for weak orders in constant amortized time', 'Greedy Universal Cycle Constructions for Weak Orders','DBLPウエブ書誌学'],
     ['Linkedin', 'これをクリックしたらすぐe-mail送られる！']],
     link: [['','https://github.com/MarsdenJacques','https://drive.google.com/file/d/1QX0TggkcIaQD4ZF1r5c0kvhsx4jLfceg/view?usp=sharing'],
     ['/#Websites','/#Games','/#Research'],
-    ['https://hiroshimageisen.netlify.app/','https://marsdensqlnotesapp.netlify.app','https://marsdenjacques.github.io/'],
+    ['https://hiroshimageisen.netlify.app/','https://marsdensqlnotesapp.netlify.app','https://tsushimaartfantasia.netlify.app/','https://marsdenjacques.github.io/'],
     ['https://marsdenjacques.itch.io/tetromivibes','https://marsdenjacques.github.io/2d-game-of-life/','https://marsdenjacques.itch.io/','https://www.kac.or.jp/wp-content/uploads/63a954ded52fd6bc624a1746aa2ffbce.pdf',''],
     ['https://www.sciencedirect.com/science/article/pii/S0012365X20301783?via%3Dihub','https://drive.google.com/file/d/1nZP3B6fD0kmuakXtf6rMIdP8ZzX9VdqV/view','https://dblp.org/pid/257/1379.html'],
-    ['https://www.linkedin.com/in/marsdenjacques/','mailto:marsdenjacques@gmail.com']]
+    ['https://www.linkedin.com/in/marsdenjacques/','mailto:marsdenjacques@gmail.com']],
+    color: ['','','','','','']
 }
 
 const Language = [English, Japanese]
 
-//make section layout component, put banners in same array, blurbs in same array, etc...
+//make section color value
 
 export default Language
